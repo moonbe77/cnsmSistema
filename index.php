a3a9c72edb61a722959fb047c52965b25ea93170
moonbe77 <moonbe77@gmail.com>
2017-09-04 11:51:58 -0300

    add titulo para test de repo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>CNSM Sistema</title>
+<title>CNSM Sistema - GITHUB VERS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="css/style.css"/>  
 <link rel="stylesheet" href="css/menu.css" type="text/css">
